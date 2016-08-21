524a4f94105000fcf324e6aedfb10bbbceb58172
lenz <lenz@lenzs-MacBook-Pro.local>
2016-08-21 21:06:22 +0200

    Use per-process 2-pass log file

--- convert_xvid.sh
+++ convert_xvid.sh
@@ -12,22 +12,31 @@ VSIZE=$(./optsize.pl "$FILEIN")
 #set SIZE=-s 480:272 -b:v 600k
 #set SIZE=-s 576:320 -b:v 800k
 
+#set VSIZE=1280x880
 #set SIZE=-s 640x272 -b:v 820k
 #SIZE="-s 640x288 -r 25 -b:v 1200k"
-SIZE="-s $VSIZE -r 25 -b:v 1200k"
+SIZE="-s $VSIZE -r 25 -b:v 1300k"
 #set SIZE= -b:v 820k -r 25
+
+#video QM
+#SIZE="-s 1280x880 -r 25 -b:v 1800k"
+
+
 
 echo "Converting $FILEIN as $SIZE"
 
-#ONEMINUTE="-t 3:00"
-VIDEO="-vtag DIVX -c:v mpeg4 -movflags faststart -f avi $SIZE"
+#ONEMINUTE="-t 13:00"
+VIDEO="-vtag DIVX -c:v mpeg4 -f avi $SIZE"
 AUDIO="-acodec libmp3lame -ac 2 -vol 256 -b:a 128k"
-MAPS="-map 0:v:0 -map 0:a:0"
+#MAPS="-map 0:0 -map 0:2"
 #MAPS="-map 0:v:0 -map 0:a:0 -filter_complex '[0:v][0:s]overlay'"
 NOAUDIO="-an"
 
-rm -f ffmpeg2pass-0.log
-$FFMPEG  -i "$FILEIN" $ONEMINUTE $VIDEO $MAPS $NOAUDIO -pass 1 -y /dev/null
-$FFMPEG  -i "$FILEIN" $ONEMINUTE $VIDEO $MAPS $AUDIO   -pass 2 "$FILEOUT"
+LOGFILE=convertxvid_$$
+
+
+rm -f $LOGFILE
+$FFMPEG  -i "$FILEIN" $ONEMINUTE $VIDEO $MAPS $NOAUDIO -pass 1 -passlogfile $LOGFILE -y /dev/null
+$FFMPEG  -i "$FILEIN" $ONEMINUTE $VIDEO $MAPS $AUDIO   -pass 2 -passlogfile $LOGFILE "$FILEOUT"
 
 
